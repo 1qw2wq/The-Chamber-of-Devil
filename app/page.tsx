509d2779ca39ecab673d63c91b10c825532efa3f
1qw2wq <135326458+1qw2wq@users.noreply.github.com>
2026-06-22 17:55:00 +0800

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import React, { useState, useEffect, useRef } from "react";
-import { SpeedInsights } from "@vercel/speed-insights/next"
 import { motion, AnimatePresence } from "motion/react";
 import {
   Shield,
